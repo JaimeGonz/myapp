ce2da14f4aaf71551f33da44adb447246b08a27d
Jaime Gonzalez <valdoc7@gmail.com>
2022-10-28 19:33:51 -0500

    Add href to the file

--- src/ImageExampleImage.js
+++ src/ImageExampleImage.js
@@ -5,7 +5,7 @@ function ImageExampleImage() {
   return (
     <div>
       <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
-      <a>Learn react</a>
+      <a href='https://reactjs.org/tutorial/tutorial.html'>Learn react</a>
     </div>
   );
 }
