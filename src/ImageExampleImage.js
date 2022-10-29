da8a2eb94d19b30eaf6de1fe9cd7860f6c005127
Jaime Gonzalez <valdoc7@gmail.com>
2022-10-28 19:29:37 -0500

    Add function instead const

--- src/ImageExampleImage.js
+++ src/ImageExampleImage.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-const ImageExampleImage = () => (
-  <Image alt="/learn react/i" src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
-)
+import React from 'react';
+
+function ImageExampleImage() {
+  return (
+    <div>
+      <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
+      <a>Learn react</a>
+    </div>
+  );
+}
 
 export default ImageExampleImage
