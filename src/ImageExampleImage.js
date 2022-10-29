e8f6a8cd756f4490883339fde3a7a8ea7b2bca69
Jaime Gonzalez <valdoc7@gmail.com>
2022-10-28 19:11:07 -0500

    Add alt line to fix app.test

--- src/ImageExampleImage.js
+++ src/ImageExampleImage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Image } from 'semantic-ui-react'
 
 const ImageExampleImage = () => (
-  <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
+  <Image alt="learn react" src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
 )
 
 export default ImageExampleImage
